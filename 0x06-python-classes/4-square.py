d30ac8b344a6db9319b71900d8e226870ba75fd8
Dixman001 <dixmaneagle@gmail.com>
2022-10-12 18:19:57 -0700

    task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -11,7 +11,12 @@ class Square:
         Args:
             size (int): represent the size of a square.
         '''
-        self.size = size
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
 
     @property
     def size(self):
