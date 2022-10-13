4c2e9ef2ce0e3bebcb81b776df1f5c69d784ee01
Dixman001 <dixmaneagle@gmail.com>
2022-10-12 17:31:33 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Define a square'''
+
+
+class Square:
+    '''square representation'''
+
+    def __init__(self, size):
+        '''Initialize square
+
+        Args:
+            size (int): represent the size of a square.
+        '''
+        self.__size = size
