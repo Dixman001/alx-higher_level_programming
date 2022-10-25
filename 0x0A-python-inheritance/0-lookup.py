36ae171feefcec8d0aa0ff6e62da3abf430f958a
Dixman001 <dixmaneagle@gmail.com>
2022-10-25 09:21:48 -0700

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    function that returns the list of available
+    attributes and methods of object.
+"""
+
+
+def lookup(obj):
+    """the function searhes for attributes and methods"""
+    return dir(obj)
