f4af42de86f3d3f636736843d879bfd1379fcae5
Dixman001 <dixmaneagle@gmail.com>
2022-11-08 09:50:48 -0800

    task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """the module returns the JSON representation of an object"""
-
+import json
 
 def to_json_string(my_obj):
     """ the function returns the JSON representation of an object"""
