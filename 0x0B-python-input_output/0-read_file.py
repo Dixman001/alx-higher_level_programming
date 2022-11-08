5aa3988d3d7f3ddc30b6e95750df200ae26f8ca5
Dixman001 <dixmaneagle@gmail.com>
2022-11-08 08:46:52 -0800

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""this module reads a file"""
+
+
+def read_file(filename=""):
+    """this function reads file in utf-8"""
+    with open("filename", encoding="utf-8") as f:
+        print(f.read(), end="")
