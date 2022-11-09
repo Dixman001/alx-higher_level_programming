fe72edbd916d3a2195e170c00f001aca918734be
Dixman001 <dixmaneagle@gmail.com>
2022-11-08 16:56:02 -0800

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""class defination"""
+
+
+class Student:
+    """Student representation"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialization"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """dictionary representation"""
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {a: getattr(self, a) for a in attrs if hasattr(self, a)}
+        return self.__dict__
